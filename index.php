65639c9993d019fd3c8f188c5607a74c89436965
Alejandro Santisteban <alejandro.santisteban@galileo.edu>
2026-03-17 04:18:13 +0000

    hola mundo

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<body style="text-align:center;">
+
+<?php
+echo "<h1>Hola mundo, soy Alejandro Santisteban</h1>";
+?>
+
+<img src="imagen.png" width="300">
+
+</body>
+</html>
